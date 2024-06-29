b57cb917f25cf4c37c8de4714c05d75497f23b84
jorpaz <giovanny.paz@outlook.com>
2024-06-28 22:58:41 -0600

    initial commit

---
+++ app/src/main/java/com/jordev/focustimer/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.jordev.focustimer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val backgroundLight = Color(0XFFD7E3FF)
+val backgroundDark = Color(0XFF101625)
+val primaryBlack = Color(0XFF000000)
+val primaryWhite = Color(0XFFFFFFFF)
+val gray = Color(0XFF888888)
+val lightGray = Color(0XFFCCCCCC)
