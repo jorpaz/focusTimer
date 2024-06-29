41d0cb57ef9116cb8e2c406d378d6c95fd02342e
jorpaz <giovanny.paz@outlook.com>
2024-06-29 14:37:56 -0600

    creacion de la UI v3

--- app/src/main/java/com/jordev/focustimer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/jordev/focustimer/ui/home/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.jordev.focustimer.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -14,14 +19,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.jordev.focustimer.R
+import com.jordev.focustimer.ui.components.AutoResizedText
+import com.jordev.focustimer.ui.components.BorderedIcon
+import com.jordev.focustimer.ui.components.CircleDot
 import com.jordev.focustimer.ui.theme.FocusTimerTheme
+import java.util.Timer
 
 @Composable
 fun HomeScreen(
 
-){
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -32,13 +42,79 @@ fun HomeScreen(
         Box(
             modifier = Modifier.fillMaxWidth(),
             contentAlignment = Alignment.TopEnd
-        ){
+        ) {
             Icon(
                 modifier = Modifier.size(FocusTimerTheme.dimens.iconSizeNormal),
                 tint = MaterialTheme.colorScheme.primary,
                 contentDescription = "Menu",
                 painter = painterResource(id = R.drawable.ic_menu)
             )
+        }
+        AutoResizedText(
+            text = "Focus Timer",
+            textStyle = MaterialTheme.typography.displayMedium.copy(
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            )
+        )
+        Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        Row {
+            CircleDot()
+            Spacer(modifier = Modifier.width(FocusTimerTheme.dimens.spacerNormal))
+            CircleDot()
+            Spacer(modifier = Modifier.width(FocusTimerTheme.dimens.spacerNormal))
+            CircleDot(color = MaterialTheme.colorScheme.tertiary)
+            Spacer(modifier = Modifier.width(FocusTimerTheme.dimens.spacerNormal))
+            CircleDot(color = MaterialTheme.colorScheme.tertiary)
+            Spacer(modifier = Modifier.width(FocusTimerTheme.dimens.spacerNormal))
+        }
+        Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        TimerSession(
+            timer = "05:00"
+        )
+    }
+}
+
+@Composable
+fun TimerSession(
+    modifier: Modifier = Modifier,
+    timer: String,
+    onIncreaseTap: () -> Unit = {},
+    onDecreaseTap: () -> Unit = {},
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            BorderedIcon(icon = R.drawable.ic_minus, onTap = onDecreaseTap)
+            Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        }
+        AutoResizedText(
+            text = timer,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(6f)
+                .align(Alignment.CenterVertically),
+            textStyle = MaterialTheme.typography.displayLarge.copy(
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            )
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            BorderedIcon(icon = R.drawable.ic_plus, onTap = onIncreaseTap)
+            Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
         }
     }
 }
@@ -48,8 +124,8 @@ fun HomeScreen(
     showBackground = true,
 )
 @Composable
-fun HomeScreenPreview(){
-    FocusTimerTheme{
+fun HomeScreenPreview() {
+    FocusTimerTheme {
         HomeScreen()
     }
 }
