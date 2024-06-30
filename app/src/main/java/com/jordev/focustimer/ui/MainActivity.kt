7298e0cb502da10637cbc37eca169d53be028437
jorpaz <giovanny.paz@outlook.com>
2024-06-29 20:28:08 -0600

    creacion de la UI v4

--- app/src/main/java/com/jordev/focustimer/ui/MainActivity.kt
+++ app/src/main/java/com/jordev/focustimer/ui/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.jordev.focustimer.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.jordev.focustimer.ui.home.HomeScreen
 import com.jordev.focustimer.ui.theme.FocusTimerTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,29 +30,12 @@ class MainActivity : ComponentActivity() {
             FocusTimerTheme(
                 windowSize = windowSize.widthSizeClass
             ) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = MaterialTheme.colorScheme.background,
+                    content = {HomeScreen()}
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FocusTimerTheme {
-        Greeting("a todos")
     }
 }
--- app/src/main/java/com/jordev/focustimer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/jordev/focustimer/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.jordev.focustimer.ui.home
 
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -18,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +29,9 @@ import com.jordev.focustimer.R
 import com.jordev.focustimer.ui.components.AutoResizedText
 import com.jordev.focustimer.ui.components.BorderedIcon
 import com.jordev.focustimer.ui.components.CircleDot
+import com.jordev.focustimer.ui.components.CustomButton
+import com.jordev.focustimer.ui.components.InformationItem
+import com.jordev.focustimer.ui.components.TimerTypeItem
 import com.jordev.focustimer.ui.theme.FocusTimerTheme
 import java.util.Timer
 
@@ -70,7 +77,48 @@ fun HomeScreen(
         }
         Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
         TimerSession(
-            timer = "05:00"
+            timer = "05:00",
+            onIncreaseTap = {
+                //TODO: Implement Logic
+            },
+            onDecreaseTap = {
+                //TODO: Implement Logic
+            }
+        )
+        Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        TimerTypeSession(
+            onTap = {
+                //TODO: Implement Logic
+            }
+        )
+        Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CustomButton(
+                text = "Start",
+                textColor = MaterialTheme.colorScheme.surface,
+                buttonColor = MaterialTheme.colorScheme.primary,
+                onTap = {
+                    //TODO: Implement Logic
+                }
+            )
+            CustomButton(
+                text = "Reset",
+                textColor = MaterialTheme.colorScheme.primary,
+                buttonColor = MaterialTheme.colorScheme.surface,
+                onTap = {
+                    //TODO: Implement Logic
+                }
+            )
+        }
+        Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        InformationSession(
+            modifier = Modifier.weight(1f),
+            round = "10",
+            time = "45:00",
         )
     }
 }
@@ -115,6 +163,82 @@ fun TimerSession(
         ) {
             BorderedIcon(icon = R.drawable.ic_plus, onTap = onIncreaseTap)
             Spacer(modifier = Modifier.height(FocusTimerTheme.dimens.spacerMedium))
+        }
+    }
+}
+
+@Composable
+fun TimerTypeSession(
+    modifier: Modifier = Modifier,
+    onTap: () -> Unit = {}
+) {
+    val gridCount = 3
+    val itemsSpacing = Arrangement.spacedBy(FocusTimerTheme.dimens.paddingNormal)
+    LazyVerticalGrid(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(FocusTimerTheme.dimens.spacerLarge),
+        columns = GridCells.Fixed(gridCount),
+        horizontalArrangement = itemsSpacing,
+        verticalArrangement = itemsSpacing,
+    ) {
+        item(
+            key = "FT"
+        ) {
+            TimerTypeItem(
+                text = "Focus Timer",
+                textColor = MaterialTheme.colorScheme.primary
+            )
+        }
+        item(
+            key = "SB"
+        ) {
+            TimerTypeItem(
+                text = "Short Break",
+                textColor = MaterialTheme.colorScheme.secondary
+            )
+        }
+        item(
+            key = "LB"
+        ) {
+            TimerTypeItem(
+                text = "Long Break",
+                textColor = MaterialTheme.colorScheme.secondary
+            )
+        }
+    }
+}
+
+@Composable
+fun InformationSession(
+    modifier: Modifier = Modifier,
+    round: String,
+    time: String,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Row(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            InformationItem(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                text = round,
+                label = "rounds"
+            )
+            Spacer(modifier = modifier
+                .fillMaxWidth()
+                .weight(1f))
+            InformationItem(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                text = time,
+                label = "time"
+            )
         }
     }
 }
